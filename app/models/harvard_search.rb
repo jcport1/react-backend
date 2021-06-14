1fab24e90230725a8c072b9d58279a419d9b410e
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-06-14 14:01:44 -0400

    Adds ENV file

--- app/models/harvard_search.rb
+++ app/models/harvard_search.rb
@@ -6,18 +6,11 @@ class HarvardSearch
 
         url = "https://api.harvardartmuseums.org/object?classification=26"
         query = "&q=title:#{query}"
-        api = "&apikey=1d2099ee-3f1e-46ff-bd4c-71d7ef213836"
+        apiKey = ENV["HARVARD_API_KEY"]
+        api = "&apikey=#{apiKey}"
         size = "&size=10"
         data = RestClient.get(url + query + api + size)
         @paintings = JSON.parse(data)["records"]
-
-        # paintings.each do |painting|
-        #     Painting.create(
-        #         title: painting["title"],
-        #         image: painting["primaryimageurl"],
-        #         artist: painting["people"][0]["name"]
-        #     )
-        # end
 
     end 
 end
